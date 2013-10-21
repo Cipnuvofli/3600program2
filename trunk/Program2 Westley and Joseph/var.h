a2b89bc1a98b26f06b3bc311fc43b8f2402516b6
westley.huebner@gmail.com <westley.huebner@gmail.com@b001342c-9ca2-b14f-498a-55cb75fce135>
2013-10-21 04:54:21 +0000

    This is everything that was turned in.

---
+++ trunk/Program2 Westley and Joseph/var.h
@@ -0,0 +1,23 @@
+struct env {
+	char name[100];
+	char value[100];
+
+	struct env *next;
+};
+
+typedef struct env *EnvP;
+
+/*
+	var is used for environmental variables
+	alias is used for alias commands
+	native is used for commands that cannot be removed
+*/
+EnvP var,alias,native;
+
+//input strings
+EnvP command;
+char input[60];
+char first[20];
+char second[20];
+char third[20];
+

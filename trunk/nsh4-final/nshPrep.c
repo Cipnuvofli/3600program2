f33b679e4abe7797a035e9c9e45bd4fdeaff61ea
westley.huebner@gmail.com <westley.huebner@gmail.com@b001342c-9ca2-b14f-498a-55cb75fce135>
2013-11-21 05:16:34 +0000

    Final version of Program 4 to turn in

---
+++ trunk/nsh4-final/nshPrep.c
@@ -0,0 +1,54 @@
+/*
+	Written by:
+	Westley Huebner
+	Joseph Penrod
+	Tyler Clements
+*/
+
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include "var.h"
+
+/*
+!!!!!!!WARNING!!!!!!!!
+setHomeDir does not allow you to debug (not sure why).
+If you would like to debug the program, comment out
+the contents of setHomeDir. The function does work,
+but for some reason it doesn't let the gdb run.
+!!!!!!!!!!!!!!!!!!!!!
+
+Returns the home directory.
+This code was taken from the Program4 description
+*/
+char* setHomeDir(){
+	struct passwd *pw = getpwuid(getuid());
+	char* homedir = pw->pw_dir;
+	return homedir;
+}
+
+//Set native commands
+setNative(){
+	nshInsert(&native,"set","set");
+	nshInsert(&native,"tes","tes");
+	nshInsert(&native,"alias","alias");
+	nshInsert(&native,"saila","saila");
+	nshInsert(&native,"echo","echo");
+	nshInsert(&native,"exit","exit");
+
+	//fill alias list for display
+	nshInsert(&alias,"set","set");
+	nshInsert(&alias,"alias","alias");
+	nshInsert(&alias,"tes","tes");
+	nshInsert(&alias,"exit","exit");
+	nshInsert(&alias,"echo","echo");
+	nshInsert(&alias,"saila","saila");
+
+	//Sets the home directory. Comment out when debugging.
+	nshInsert(&var,"Path",setHomeDir());
+
+}
+
---
+++ trunk/nsh4-final/var.h
@@ -0,0 +1,28 @@
+/*
+	Written by:
+	Westley Huebner
+	Joseph Penrod
+	Tyler Clements
+*/
+
+struct env {
+	char name[100];
+	char value[200];
+	struct env *next;
+};
+
+typedef struct env *EnvP;
+
+/*
+	var is used for environmental variables
+	alias is used for alias commands
+	native is used for commands that cannot be removed
+*/
+EnvP var,alias,native;
+
+//input strings
+EnvP command;
+char input[80];
+char first[20];
+char second[120];
+char third[120];
